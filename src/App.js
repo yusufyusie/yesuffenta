4757dc0e99581255dcf5b22353e25e5e3475bb32
yusufyusie <yesuf023@gmail.com>
2024-03-28 13:40:15 +0300

    Remove an used file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <button type="button" className="btn btn-primary">Click me</button>
+    </div>
   );
 }
 
