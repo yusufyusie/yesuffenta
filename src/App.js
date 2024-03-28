c62fbabf6003f3523392fadf477800c4b0f3adca
yusufyusie <yesuf023@gmail.com>
2024-03-28 13:22:37 +0300

    Add index file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './components/Navigation';
-import Masthead from './components/Masthead';
 
-const App = () => (
-  <div>
-    <Navigation />
-    <Masthead />
-    {/* Add other components here */}
-  </div>
-);
+function App() {
+  return (
+    <div className="App" />
+  );
+}
 
 export default App;
