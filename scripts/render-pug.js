2bd0ebaf5d7e2906ca8d2c0f7dcdae071a788635
yusufyusie <yesuf023@gmail.com>
2024-03-29 18:34:29 +0300

    Implement Pug file rendering and HTML prettification

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -5,48 +5,31 @@ const pug = require('pug');
 const sh = require('shelljs');
 const prettier = require('prettier');
 
-module.exports = async function renderPug(filePath) {
+module.exports = function renderPug(filePath) {
     const destPath = filePath.replace(/src\/pug\//, 'public/').replace(/\.pug$/, '.html');
     const srcPath = upath.resolve(upath.dirname(__filename), '../src');
 
     console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
-    
-    // Render the Pug file
-    const renderPromise = new Promise((resolve, reject) => {
-        pug.renderFile(filePath, {
-            doctype: 'html',
-            filename: filePath,
-            basedir: srcPath
-        }, (err, html) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(html);
-            }
-        });
+    const html = pug.renderFile(filePath, {
+        doctype: 'html',
+        filename: filePath,
+        basedir: srcPath
     });
 
-    try {
-        const html = await renderPromise; // Wait for the Promise to resolve
-        const destPathDirname = upath.dirname(destPath);
-        
-        if (!sh.test('-e', destPathDirname)) {
-            sh.mkdir('-p', destPathDirname);
-        }
-
-        // Prettify the HTML and write to the file
-        const prettified = prettier.format(html, {
-            printWidth: 1000,
-            tabWidth: 4,
-            singleQuote: true,
-            proseWrap: 'preserve',
-            endOfLine: 'lf',
-            parser: 'html',
-            htmlWhitespaceSensitivity: 'ignore'
-        });
-
-        fs.writeFileSync(destPath, prettified);
-    } catch (error) {
-        console.error('Error rendering Pug file:', error);
+    const destPathDirname = upath.dirname(destPath);
+    if (!sh.test('-e', destPathDirname)) {
+        sh.mkdir('-p', destPathDirname);
     }
+
+    const prettified = prettier.format(html, {
+        printWidth: 1000,
+        tabWidth: 4,
+        singleQuote: true,
+        proseWrap: 'preserve',
+        endOfLine: 'lf',
+        parser: 'html',
+        htmlWhitespaceSensitivity: 'ignore'
+    });
+
+    fs.writeFileSync(destPath, prettified);
 };
